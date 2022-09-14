2b2fe7a17e7a8bdf8e0d8f84eb4296362e0134a4
stepanamz <stepanamz@mail.ru>
2022-09-14 14:03:16 +0700

    Деление с дробной частью

--- Program.cs
+++ Program.cs
@@ -3,7 +3,14 @@
 // string username = Console.ReadLine();
 // Console.Write("Привет, " + username);
 
-int numberA = 3;
-int numberB = 5;
-int sum = numberA + numberB;
+// Сложение двух целых чисел
+// int numberA = 3;
+// int numberB = 5;
+// int sum = numberA + numberB;
+// Console.WriteLine(sum);
+
+// Деление с дробной частью
+double numberA = 12;
+double numberB = 5;
+double sum = numberA / numberB;
 Console.WriteLine(sum);
