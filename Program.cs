b9ba6b647139d36600c42be40f4a4be17cc64a41
stepanamz <stepanamz@mail.ru>
2022-09-14 13:50:26 +0700

    Создал сложение двух чисел

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
-﻿
-Console.Write("Введите ваше имя ");
-string username = Console.ReadLine();
-Console.Write("Привет, " + username);
+﻿// Hello User
+// Console.Write("Введите ваше имя ");
+// string username = Console.ReadLine();
+// Console.Write("Привет, " + username);
+
+int numberA = 3;
+int numberB = 5;
+int sum = numberA + numberB;
+Console.WriteLine(sum);
